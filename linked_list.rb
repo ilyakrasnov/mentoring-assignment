5ef001cd1074fb88f71a25e207fd4791f1f06ff2
Ilya Krasnov <ilya.krasnov@gmail.com>
2015-11-12 21:13:07 +0100

    Add revlist

--- linked_list.rb
+++ linked_list.rb
@@ -8,6 +8,7 @@ class LinkedListNode
 end
 
 def print_values(list_node)
+list_node.inspect
   print "#{list_node.value} --> "
   if list_node.next_node.nil?
     print "nil\n"
@@ -16,24 +17,6 @@ def print_values(list_node)
     print_values(list_node.next_node)
   end
 end
-
-
-def reverse_list(list)
-    # ADD CODE HERE
-
-    while list
-        # ADD CODE HERE
-        list = list.next_node
-    end
-
-    # ADD CODE HERE
-end
-
-node1 = LinkedListNode.new(37)
-node2 = LinkedListNode.new(99, node1)
-node3 = LinkedListNode.new(12, node2)
-
-print_values(node3)
 
 class Stack
   attr_reader :data
@@ -45,7 +28,7 @@ class Stack
   # Push an item onto the stack
   def push(element)
     # IMPLEMENT ME!
-    updated_stack = LinkedList.new(element, @data)
+    updated_stack = LinkedListNode.new(element.value, @data)
     @data = updated_stack
   end
 
@@ -53,9 +36,39 @@ class Stack
   # Remove the last item that was pushed onto the
   # stack and return it to the user
   def pop
-    item_to_pop = @data.value
-    @data = @data.next_node
+    item_to_pop = @data.nil? ? nil : @data.value
+    @data = @data.nil? ? LinkedListNode.new(nil) : @data.next_node
     item_to_pop
   end
-
 end
+
+def reverse_list(list)
+    stack = Stack.new
+    revlist = nil
+
+    while list
+      puts stack.pop
+      list = list.next_node
+    end
+
+    while stack.data
+      revlist = LinkedListNode.new(stack.pop, revlist)
+    end
+
+    revlist
+    # ADD CODE HERE
+end
+
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+print_values(node3)
+
+puts "-------"
+
+revlist = reverse_list(node3)
+puts revlist.inspect
+print_values(revlist)
+
